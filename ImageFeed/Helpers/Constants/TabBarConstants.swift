590aeb6a3be88952848db76cb9ce4950a0468602
FaNtast2912 <z_maks@inbox.ru>
2024-10-13 17:05:54 +0300

    Profile View Controller added from code, UIWindow settings added

---
+++ ImageFeed/Helpers/Constants/TabBarConstants.swift
@@ -0,0 +1,11 @@
+//
+//  TabBarConstants.swift
+//  ImageFeed
+//
+//  Created by Maksim Zakharov on 13.10.2024.
+//
+
+enum TapBarIdentifiers: String {
+    case imagesListViewController = "ImagesListViewController"
+    case profileViewController = "ProfileViewController"
+}
--- ImageFeed/ViewConrollers+Navigation/TabBarController.swift
+++ ImageFeed/ViewConrollers+Navigation/TabBarController.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
+    // MARK: - IB Outlets
+
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - Overrides Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: TapBarIdentifiers.imagesListViewController.rawValue)
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+                       title: "",
+                       image: UIImage(named: "profileScreenActive"),
+                       selectedImage: nil
+                   )
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+    // MARK: - IB Actions
+
+    // MARK: - Public Methods
+
+    // MARK: - Private Methods
     
 }
